93536d1e3181a6a9be1005d2ca9dcd69290be226
HaykMelqonyan0110 <hayk.melqonyan0110@gmail.com>
2023-02-24 19:58:35 +0400

    Starting Project

---
+++ Galleria/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("", include("homepage.urls")),
+    path('login/', views.LoginView(template_name='user/login.html'), name='login'),
+    path('logout/', views.LogoutView(template_name='user/logout.html'), name='logout')
+]
